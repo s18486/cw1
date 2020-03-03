49ffcb17ca65a0db8779a388e205d5239550940a
Artur Ostapuk <s18486@pjwstk.edu.pl>
2020-03-03 11:34:23 +0100

    Pierwszy commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Console.Writeline("hello world!");
+            //var person = new Person { imie = "john" };
+            var url = args.Length > 0 ? args[0]: "https://pja.edu.pl";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var tmp = await response.Content.ReadAsStringAsync();
+                var reg = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = reg.Matches(tmp);
+
+                foreach(var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+        }          
+    }
+}
